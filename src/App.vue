f00ef5c1f8591403158bcaa92af9dd2a697d1597
gabrielweinbrenner <gabriel.weinbrenner@gmail.com>
2021-05-26 14:34:49 -0400

    Saving feature

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,32 @@
 <template>
-  <div id="app">
-    <Book msg="Counter App"/>
-  </div>
+    <div id="app">
+        <Book msg="Counter App" :saved="this.saved" />
+    </div>
 </template>
 
 <script>
-import Book from './components/Book.vue'
+import Book from "./components/Book.vue";
 
 export default {
-  name: 'App',
-  components: {
-    Book
-  }
-}
+    name: "App",
+    data() {
+        return {
+            saved: [],
+        };
+    },
+    components: {
+        Book,
+    },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
--- src/components/Book.vue
+++ src/components/Book.vue
@@ -1,60 +1,96 @@
 <template>
-  <div class="hello">
-    <!-- <h1>{{ msg }}</h1>
+    <div class="hello">
+        <!-- <h1>{{ msg }}</h1>
     <h2>{{ count }}</h2>
     <button v-on:click="count++">Add + 1</button> -->
-    <div>
-      <p>What type of books to look for?</p>
-      <input v-model="category" v-on:keyup.enter="getName">
-      <div v-for="book in data.items" :key="book.id">
-        <h3>{{book.volumeInfo.title}}</h3>
-        <img :src="book.volumeInfo.imageLinks.thumbnail" >
-      </div>
+        <div>
+            <p>What type of books to look for?</p>
+            <input v-model="category" v-on:keyup.enter="getName" />
+            <!-- <div v-for="book in data" :key="book.id">
+					<h3>{{ book.name }}</h3>
+					<img :src="book.picture" />
+				</div> -->
+
+            <div v-if="loaded && data.length != currentElement" class="book">
+                <p>{{ data[currentElement].name }}</p>
+                <img :src="data[currentElement].picture" />
+            </div>
+            <div v-else class="book">
+                <p>Enter more to discover more</p>
+            </div>
+            <div v-if="data.length != currentElement">
+                <button @click="nextBook(true)">YES</button>
+                <button @click="nextBook(false)">NO</button>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  beforeMount(){
-    this.getName()
-  },
-  methods: {
-    async getName(){
-      const res = await fetch("https://www.googleapis.com/books/v1/volumes?q=" + this.category)
-      const data = await res.json()
-      this.data = data
-      console.log(this.data)
-    }
-  },
-  data(){
-    return {
-      count: 0,
-      category: "cooking",
-      data: "",
-    }
-  },
-  props: {
-    msg: String
-  }
-}
+    name: "Book",
+    beforeMount() {
+        this.getName();
+    },
+    methods: {
+        async getName() {
+            const res = await fetch(
+                "https://www.googleapis.com/books/v1/volumes?q=" + this.category
+            );
+            const data = await res.json();
+            this.data = data.items.map((v) => {
+                return {
+                    id: v.id,
+                    name: v.volumeInfo.title,
+                    picture: v.volumeInfo.imageLinks.thumbnail,
+                    isSaved: false,
+                };
+            });
+            this.loaded = true;
+        },
+        nextBook(isYes) {
+            if (isYes) {
+                this.saved.push(this.data[this.currentElement]);
+            }
+            this.currentElement++;
+            if (this.currentElement == this.data.length) {
+                this.data = [];
+                this.currentElement = 0;
+            }
+        },
+    },
+    data() {
+        return {
+            currentElement: 0,
+            loaded: false,
+            category: "cooking",
+            data: [],
+        };
+    },
+    props: {
+        msg: String,
+        saved: Array,
+    },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
-  margin: 40px 0 0;
+    margin: 40px 0 0;
 }
 ul {
-  list-style-type: none;
-  padding: 0;
+    list-style-type: none;
+    padding: 0;
 }
 li {
-  display: inline-block;
-  margin: 0 10px;
+    display: inline-block;
+    margin: 0 10px;
 }
 a {
-  color: #42b983;
+    color: #42b983;
+}
+.book {
+    height: 250px;
 }
 </style>
